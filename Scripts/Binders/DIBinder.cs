dfd96ddfd808f3b53a1f84ceaa2984114a6fa538
Manh Nguyen <manhnb1911@gmail.com>
2024-06-28 17:40:24 +0700

    fix binders

--- Scripts/Binders/DIBinder.cs
+++ Scripts/Binders/DIBinder.cs
@@ -3,11 +3,16 @@
 namespace UniT.Audio
 {
     using UniT.DI;
+    using UniT.Logging;
+    using UniT.ResourceManagement;
 
     public static class DIBinder
     {
         public static void AddAudioManager(this DependencyContainer container)
         {
+            if (container.Contains<IAudioManager>()) return;
+            container.AddLoggerManager();
+            container.AddResourceManagers();
             container.AddInterfaces<AudioManager>();
         }
     }
--- Scripts/Binders/ZenjectBinder.cs
+++ Scripts/Binders/ZenjectBinder.cs
@@ -2,12 +2,17 @@
 #nullable enable
 namespace UniT.Audio
 {
+    using UniT.Logging;
+    using UniT.ResourceManagement;
     using Zenject;
 
     public static class ZenjectBinder
     {
         public static void BindAudioManager(this DiContainer container)
         {
+            if (container.HasBinding<IAudioManager>()) return;
+            container.BindLoggerManager();
+            container.BindResourceManagers();
             container.BindInterfacesTo<AudioManager>().AsSingle();
         }
     }
